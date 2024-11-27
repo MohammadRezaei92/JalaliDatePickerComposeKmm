c594109390dde8818139d884b13392bb7217358d
Mohammad <moreat92@gmail.com>
2024-11-27 08:44:16 +0330

    Remove zip

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,27 +7,3 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
 }
 
-tasks.register<Zip>("zipIosArm64Framework") {
-    group = "build"
-    description = "Zips the iOS Arm64 framework for local use"
-    from(layout.buildDirectory.dir("bin/iosArm64/releaseFramework/ComposeApp.framework"))
-    destinationDirectory.set(file("$buildDir/outputs/iosFrameworks"))
-    archiveFileName.set("ComposeApp-iosArm64.framework.zip")
-}
-
-tasks.register<Zip>("zipIosX64Framework") {
-    group = "build"
-    description = "Zips the iOS X64 framework for local use"
-    from(layout.buildDirectory.dir("bin/iosX64/releaseFramework/ComposeApp.framework"))
-    destinationDirectory.set(file(layout.buildDirectory.dir("outputs/iosFrameworks")))
-    archiveFileName.set("ComposeApp-iosX64.framework.zip")
-}
-
-tasks.register<Zip>("zipIosSimulatorArm64Framework") {
-    group = "build"
-    description = "Zips the iOS Simulator Arm64 framework for local use"
-    from(layout.buildDirectory.dir("bin/iosSimulatorArm64/releaseFramework/ComposeApp.framework"))
-    destinationDirectory.set(file(layout.buildDirectory.dir("outputs/iosFrameworks")))
-    archiveFileName.set("ComposeApp-iosSimulatorArm64.framework.zip")
-}
-
